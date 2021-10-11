afee148cf5a127f92d8d99a1a74c2c75b2a6ae79
serhat potur <serhat_potur@hotmail.com>
2021-10-11 22:19:38 +0300

    34 Bitti | Register sayfası oluşturuldu ve SweatAlert kullanıldı

---
+++ CoreBlogApp/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrate;
+using DataAccess.EntityFramework;
+using Entity.Concrate;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Writer writer)
+        {
+            writer.WriterStatus = true;
+            writerManager.Add(writer);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
